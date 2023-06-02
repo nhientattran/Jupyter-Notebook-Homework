b06c72648ee7ab47d450b9f9f74405205b09c0a4
nhientattran <132933625+nhientattran@users.noreply.github.com>
2023-06-01 23:12:39 -0500

    week 2 day 4

---
+++ W2D4_1a.py
@@ -0,0 +1,3 @@
+def square_footage(house_length, house_width):
+    house_area = house_length * house_width
+    return f"The area of the house is {house_area} square foot"
---
+++ W2D4_1b.py
@@ -0,0 +1,3 @@
+def cir_cir(radius):
+    output = 2*3.141592653589793*radius
+    return f"The circumference of a circle is {output}"
